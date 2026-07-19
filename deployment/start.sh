27e35df1e7c9b28a5687ca085f08abbede3638d3
Syafiqatt <syafiqaja030705@gmail.com>
2026-07-19 22:03:02 +0700

    Update start.sh

--- deployment/start.sh
+++ deployment/start.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 set -e
 
-echo "Starting ML Service..."
-
+echo "Masuk ke ML Service..."
 cd /app/ml-service
+
+# Download model jika belum ada
+if [ ! -f "career_recommender_model.pkl" ]; then
+    echo "Downloading ML model..."
+    python3 -m gdown --id 1oxJa6tXs8rjAAOy-fzyS4cyNKchggvwl -O career_recommender_model.pkl
+fi
+
+echo "Menjalankan ML Service..."
 python3 -m uvicorn app:app --host 0.0.0.0 --port 8000 &
 
-echo "Starting Backend..."
-
+echo "Menjalankan Backend..."
 cd /app/backend
 npm start
